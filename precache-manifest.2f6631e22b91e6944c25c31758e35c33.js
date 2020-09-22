68f06ef86997937b8bac767b62e489dba1deb179
Vikasdeep Chaudhary <vikasdeep3098@gmail.com>
2020-09-22 19:39:54 +0530

    Updates

---
+++ precache-manifest.2f6631e22b91e6944c25c31758e35c33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20ac2bc6ab42ed84d36b413b56a81d43",
+    "url": "/react-weather-api-live/index.html"
+  },
+  {
+    "revision": "2fc7dd92f62c2bf6731e",
+    "url": "/react-weather-api-live/static/css/main.05487bed.chunk.css"
+  },
+  {
+    "revision": "55d587c08e161ec4a8e7",
+    "url": "/react-weather-api-live/static/js/2.eaed646f.chunk.js"
+  },
+  {
+    "revision": "9a8a64d30a81695e7691dd446a703cba",
+    "url": "/react-weather-api-live/static/js/2.eaed646f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fc7dd92f62c2bf6731e",
+    "url": "/react-weather-api-live/static/js/main.9bd335ad.chunk.js"
+  },
+  {
+    "revision": "8bd07cd87b6ad17693cd",
+    "url": "/react-weather-api-live/static/js/runtime-main.2dd316e7.js"
+  },
+  {
+    "revision": "f56731126cd9c6d519a0f557766218f4",
+    "url": "/react-weather-api-live/static/media/alberto-restifo-BNHcxk1CDdw-unsplash.f5673112.jpg"
+  }
+]);
